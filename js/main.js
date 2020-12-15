681647225a54580b762473d0d1fd0e8c33f4f989
gabriela.cabrera@smartifice.com <gabriela.cabrera@smartifice.com>
2020-12-15 11:54:17 -0600

    update

--- js/main.js
+++ js/main.js
@@ -6,6 +6,10 @@
  * @copyright       Derechos reservados © 2020
  */
 
+
+
+
+
 $(document).ready(()=>{
     // Validar campos cuando su valor cambie, eventos keypress/change
     $('input').on('input', (e) => {
@@ -17,28 +21,55 @@ $(document).ready(()=>{
     });
 });
 
+var indicadores;
+var pointer;
+var historial= [];
+console.log("obteniendo historial 1",historial)
+historial = JSON.parse(localStorage.getItem('registro')) || [];
+console.log("obteniendo historial 2",historial)
+console.log(historial)
+
 /**
  * Calcular el crecimiento natural y absoluto de cualquier población
  * a través del tiempo. Los indicadores a considerar son: población
  * inicial, años a proyectar, tasa de natalidad, tasa de motalidad y
  * saldo migratorio (tasa de emigrantes y tasa de inmigrantes).
  */
-function calcularCP() {
+function calcularCP(config) {
     boolPi = validarCampoVacio('txt-pi');
     boolAn = validarCampoVacio('txt-an');
     boolTn = validarCampoVacio('txt-tn');
     boolTm = validarCampoVacio('txt-tm');
     boolEm = validarCampoVacio('txt-em');
     boolIn = validarCampoVacio('txt-in');
-    if (boolPi && boolAn && boolTn && boolTm && boolEm && boolIn) {
-        let indicadores = {
-            poblacionInicial: parseFloat(document.getElementById('txt-pi').value),
-            anios: parseFloat(document.getElementById('txt-an').value),
-            tasaNatalidad: parseFloat(document.getElementById('txt-tn').value),
-            tasaMortalidad: parseFloat(document.getElementById('txt-tm').value),
-            tasaEmigracion: parseFloat(document.getElementById('txt-em').value),
-            tasaInmigracion: parseFloat(document.getElementById('txt-in').value)
-        };
+    if (boolPi && boolAn && boolTn && boolTm && boolEm && boolIn || config.flag) {
+        if(flag){
+            indicadores = {
+                nombre:"",
+                date : "",
+                poblacionInicial: parseFloat(document.getElementById('txt-pi').value),
+                anios: parseFloat(document.getElementById('txt-an').value),
+                tasaNatalidad: parseFloat(document.getElementById('txt-tn').value),
+                tasaMortalidad: parseFloat(document.getElementById('txt-tm').value),
+                tasaEmigracion: parseFloat(document.getElementById('txt-em').value),
+                tasaInmigracion: parseFloat(document.getElementById('txt-in').value)
+            };
+    
+        }else{
+            indicadores = {
+                nombre:historial[config.id].nombre,
+                date : historial[config.id].date,
+                poblacionInicial:  parseFloat(historial[config.id].poblacionInicial),
+                anios:  parseFloat(historial[config.id].anios),
+                tasaNatalidad:  parseFloat(historial[config.id].tasaNatalidad),
+                tasaMortalidad: parseFloat(historial[config.id].tasaMortalidad),
+                tasaEmigracion:  parseFloat(historial[config.id].tasaEmigracion),
+                tasaInmigracion: parseFloat(historial[config.id].tasaInmigracion)
+            };
+    
+        }
+       
+        
         // Obtener valores iniciales y crear arreglo de datos
         let cn = indicadores.poblacionInicial;
         let ca = indicadores.poblacionInicial;
@@ -76,6 +107,7 @@ function calcularCP() {
         console.log('Formulario inválido');
     }
 }
+
 
 /**
  * Reiniciar indicadores para el cálculo de crecimiento poblacional.
@@ -117,3 +149,239 @@ function validarCampoVacio(id){
         return true;
     }
 }
+
+function ventanaModal(id, data) {
+    data=data || {};                            // si no existe data, creamos un objeto vacío para evitar posteriores errores
+    id="modal-"+id;                             // añadimos "modal-" a la id para evitar conflictos con otros elementos
+    if (document.getElementById(id)==null) {    // crear elemento si no existe
+      var d=document.createElement("div");
+      d.className="jmgmodal";                   // clase para estilos con CSS
+      d.id=id;
+      // creamos el panel interior
+      var p=document.createElement("div");
+      p.className="panel";
+      // creamos los componentes de la cabecera: título y botón de cerrar
+      var t=document.createElement("div");
+      t.className="title";
+      var cl=document.createElement("div");
+      cl.className="close";
+      cl.innerHTML='&times;';
+      // cerramos y vaciamos la modal al pulsar el botón X
+      cl.addEventListener('click',function(ev) {
+        ev.preventDefault();
+        var dTop=this.parentNode.parentNode;
+        dTop.classList.remove("visible");
+        dTop.querySelector(".panel .content").innerHTML='';
+      });
+      // creamos la caja donde se mostrará el contenido
+      var ct=document.createElement("div");
+      ct.className="content";
+      ct.id="content-modal"
+
+      
+      var f=document.createElement("div");
+      /* finalmente, añadimos "t", "cl" y "ct" (título, botón cerrar y div contenido) a "p" (panel interior), 
+      éste lo añadimos a "d" (div principal, para oscurecer el fondo), y "d" lo añadimos al body de la página */
+      p.appendChild(t);p.appendChild(cl);p.appendChild(ct);d.appendChild(p);
+      document.body.appendChild(d);
+    }
+    // guardamos cada componente en una variable
+    var mod=document.getElementById(id),
+    p=mod.querySelector(".panel"),
+    t=mod.querySelector(".panel .title"),
+    ct=mod.querySelector(".panel .content");
+    // rellenamos los datos
+    t.innerHTML=data.title || '';
+    ct.innerHTML=data.content || '';
+    // comprobamos que el número es válido antes de añadirlo
+    if (!isNaN(data.width)) p.style.maxWidth=data.width+'px';
+    if (!isNaN(data.height)) p.style.maxHeight=data.height+'vh';
+    /* esperamos unos milisegundos para que se genere, y añadimos la clase .visible para mostrarla desde CSS */
+    setTimeout(function(){
+      mod.classList.add("visible");
+    },50);
+    console.log(data.action)
+     if(data.action == 2){
+        generateContent()
+     }else{
+         if(data.action == 3){
+             generateSaveSim()
+         }else{
+             if(data.action == 1){
+                 generateInfo()
+                
+             }
+         }
+     }
+     
+  }
+
+  function generateContent(){
+      console.log("historial",historial)
+    document.getElementById("content-modal").innerHTML="";
+    for(let i=0; i<historial.length;i++){
+        console.log("hola")
+        document.getElementById("content-modal").innerHTML+=` <div class="item-list" onclick="selectItem(${i})" id="">
+        <div class="item-content item row" >
+            <div class="col-6 content-element1">
+                <span>${historial[i].nombre}</span>
+            </div>
+            <div class="col-4 content-element2">
+                <span>${historial[i].date}</span>
+            </div>
+            <div class="col-2" >
+                <button class="content-button" onclick="editRecord(${i})">
+                <i class="fas fa-edit"></i></button>
+        </div>
+        <div class="content-edit mt-2 d-none" id="edit-${i}">
+            <div class="row">
+                <div class="col-4 mb-3">
+                    <span class="sp-form">Población inicial</span>
+                   <input type="number" placeholder="" class="form-control" id="inicial-p-${i}">
+                </div>
+                <div class="col-4  mb-3">
+                    <span class="sp-form">Años a proyectar</span>
+                    <input type="number" placeholder="" class="form-control"  id="anios-p-${i}">
+                 </div>
+                 <div class="col-4  mb-3">
+                 <span class="sp-form">Tasa natalidad</span>
+                    <input type="number" placeholder="" class="form-control" id="natalidad-p-${i}">
+                 </div>
+            </div>
+            <div class="row">
+                <div class="col-4  mb-3">
+                     <span class="sp-form">Tasa mortalidad</span>
+                    <input type="number" placeholder="" class="form-control" id="mortalidad-p-${i}">
+                 </div>
+                <div class="col-4  mb-3">
+                    <span class="sp-form">Tasa emigración</span>
+                    <input type="number" placeholder="" class="form-control" id="emigracion-p-${i}">
+                </div>
+                <div class="col-4  mb-3">
+                    <span class="sp-form">Tasa inmigración</span>
+                    <input type="number" placeholder="" class="form-control" id="inmigracion-p-${i}">
+                </div>
+            </div>
+            <div class="row">
+            <div class="col-12">
+                <span class="sp-form">Nombre del escenario</span>
+                <input type="text" placeholder="" class="form-control" id="name-sim-${i}">
+            </div>
+            </div>
+            <button class="button__main button__green button-save-2" onclick="updateSimulation(${i})">
+             Modificar
+            </button>
+        </div>
+        </div>
+        `;
+    }
+
+    document.getElementById("content-modal").innerHTML+=`<button class="button__main button__blue--dark  button-save-2" onclick="loadSimulation()">
+    Cargar
+   </button>`
+    
+  }
+
+  function  generateSaveSim(){
+    document.getElementById("content-modal").innerHTML="";
+    document.getElementById("content-modal").innerHTML+=`
+    <input type="text" class="form-control" style="margin-top:2rem" placeholder="Nombre del escenario" id="name-simulation">
+    <button class="button__main button__green button-save-2" onclick="saveSimulation()">
+        Guardar
+    </button>
+    `;
+  }
+
+  function saveSimulation(){
+      console.log("guardando")
+        indicadores.nombre=document.getElementById("name-simulation").value;
+        indicadores.date=generateDate()
+        console.log(indicadores)
+        historial.push(indicadores)
+        indicadores = {}
+        localStorage.setItem('registro', JSON.stringify(historial));
+
+        restablecer()
+  }
+
+  function generateDate(){
+    n =  new Date();
+    y = n.getFullYear();
+    m = n.getMonth() + 1;
+    h = n.getHours();
+    ms = n.getMinutes();
+    d = n.getDate();
+    return  d + "/" + m + "/" + y + " "+h+ms 
+  }
+
+
+  
+  var flag = false;
+  function editRecord(id){
+    if(!flag){
+        flag=true;
+        document.getElementById(`edit-${id}`).classList.remove("d-none");
+        document.getElementById(`edit-${id}`).classList.add("d-block");
+        setDataEdit(id)
+    }else{
+        flag=false;
+        document.getElementById(`edit-${id}`).classList.remove("d-block");
+        document.getElementById(`edit-${id}`).classList.add("d-none")
+    }
+    
+  }
+
+  function updateSimulation(id){
+    indicadores_edit = {
+        nombre: document.getElementById(`name-sim-${id}`).value,
+        date : generateDate(),
+        poblacionInicial: parseFloat(document.getElementById(`inicial-p-${id}`).value),
+        anios: parseFloat(document.getElementById(`anios-p-${id}`).value),
+        tasaNatalidad: parseFloat(document.getElementById(`natalidad-p-${id}`).value),
+        tasaMortalidad: parseFloat(document.getElementById(`mortalidad-p-${id}`).value),
+        tasaEmigracion: parseFloat(document.getElementById(`emigracion-p-${id}`).value),
+        tasaInmigracion: parseFloat(document.getElementById(`inmigracion-p-${id}`).value)
+    };
+    historial[id]=indicadores_edit;
+    localStorage.setItem('registro', JSON.stringify(historial));
+    indicadores_edit={}
+    editRecord(id);
+
+  }
+
+
+  function  setDataEdit(id){
+    document.getElementById(`name-sim-${id}`).value=historial[id].nombre;
+    document.getElementById(`inicial-p-${id}`).value=historial[id].poblacionInicial;
+    document.getElementById(`natalidad-p-${id}`).value=historial[id].tasaNatalidad;
+    document.getElementById(`anios-p-${id}`).value=historial[id].anios;
+    document.getElementById(`mortalidad-p-${id}`).value=historial[id].tasaMortalidad;
+    document.getElementById(`emigracion-p-${id}`).value=historial[id].tasaEmigracion;
+    document.getElementById(`inmigracion-p-${id}`).value=historial[id].tasaInmigracion;
+    
+  }
+
+  function selectItem(id){
+    poin=id;
+  }
+
+  function loadSimulation(){
+      console.log(poin)
+      let config={
+          flag:true,
+          id:poin
+      }
+    calcularCP(config)
+  }
+
+  function  generateInfo(){
+    document.getElementById("content-modal").innerHTML+=`
+    <p class="text_info ">
+    El siguiente modelo de crecimiento poblacional proyecta la cantidad de habitantes en base al crecimiento poblacional y el crecimiento poblacional absoluto utilizando un algoritmo recursivo que permitirá realizar la proyección en años en base al resultado del crecimiento del año anterior y siguiendo la misma tasa de natalidad, mortalidad, emigración e inmigración.
+    </p>
+    <p>
+    <strong>Modelo Empleado</strong>
+    <img class="img_model" src="img/model.JPG">  </p>`
+  }
+
+  
